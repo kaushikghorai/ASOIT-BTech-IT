f21b7941e71877971e140ef30bdaf70c14441a09
Kaushik Ghorai <kaushikghorai@ubuntu.linux>
2025-04-03 01:38:54 +0530

    modified Practicle7.c

--- LDP-II/manual/practical7.c
+++ LDP-II/manual/practical7.c
@@ -2,18 +2,29 @@
 #include<stdlib.h>
 void main(){
     FILE *fnum, *fodd, *feven;
-    int num;
-    fnum = fopen("numbers.txt","w");
-    feven = fopen("even.txt","w");
-    fodd = fopen("odd.txt","w");
-    int innum;
-    for(int i=0;i<10;i++){
-        scanf("%d", &innum);
-        fprintf(fnum, "%d\n", innum);
+    char num, innum;
+    fnum = fopen("numbers.txt", "w+");
+    feven = fopen("even.txt", "w+");
+    fodd = fopen("odd.txt", "w+");
+    printf("Enter 10 numbers (0-9):\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%c", &innum);
+        putc(innum, fnum);
     }
-    while(fscanf(fnum, "%d", &num) != EOF){
-        if(num%2==0) fprintf(feven,"%d\n",num);
-        else fprintf(fodd,"%d",num);
+    rewind(fnum);
+    while ((num = getc(fnum)) != EOF) {
+        if (num % 2 == 0) putc(num, feven);
+        else putc(num, fodd);
+    }
+    printf("Numbers in even.txt:\n");
+    rewind(feven);
+    while ((num = getc(feven)) != EOF) {
+        printf("%c ", num);
+    }
+    printf("\nNumbers in odd.txt:\n");
+    rewind(fodd);
+    while ((num = getc(fodd)) != EOF) {
+        printf("%c ", num);
     }
     fclose(fnum);
     fclose(fodd);
